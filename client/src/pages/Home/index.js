41dcc9d0e4bdcbe4f9564b63deffa84178c0ae21
Nathan Gianelli <nathan.gianelli98@gmail.com>
2020-04-08 11:40:12 -0400

    added contact form code

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../../components/Hero"
-import Highlight from "../../components/Highlight"
+import Process from "../../components/Process"
 import Footer from "../../components/Footer"
 import "../Home/home.css"
 import Gallery from "../../components/Gallery"
@@ -10,8 +10,8 @@ function Home() {
     <div>
       <Hero />
       <Gallery />
-      {/* <Highlight /> */}
-      {/* <Footer /> */}
+      <Process />
+      <Footer />
     </div>
   );
 }
