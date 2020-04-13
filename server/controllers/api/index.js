9c344c7cbdd551bf42b6cbc77be360fbaf5ec5f6
Nathan Gianelli <nathan.gianelli98@gmail.com>
2020-04-13 16:15:44 -0400

    nodemailer code added to storiesControllers and nodemailer code removed from storiesForm

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,6 +1,7 @@
 const apiControllers = require('express').Router();
 
 apiControllers.use('/users', require('./usersController'));
+apiControllers.use('/stories', require('./storiesController'));
 apiControllers.use('/secrets', require('./secretsController'));
 
 module.exports = apiControllers;
