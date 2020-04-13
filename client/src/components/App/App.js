dbf8db39a5ec097ae48e696a92da7997711a5c9e
Jethan16 <ethan.nel.jones@gmail.com>
2020-04-13 19:10:06 -0400

    fixing build issue

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -16,7 +16,6 @@ import Vision from "../../pages/Vision";
 import Form from "../../components/storiesForm";
 import Contact from "../../components/contactForm";
 
-
 import snkrsBanner from "../../images/logos/SwS_Logo_Full.png";
 import "./App.css";
 function App() {
