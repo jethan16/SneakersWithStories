5d9b70ba965097a3377d4933e6b85cbf5d1ef5b0
Jethan16 <ethan.nel.jones@gmail.com>
2020-04-13 20:29:22 -0400

    resolving build issue

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/ProjectThree", {
+mongoose.connect("mongodb://localhost/ProjectThree" || MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
